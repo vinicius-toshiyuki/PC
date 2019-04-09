70fdf9abcbb5cc5abdc4219f3949bdb88af9a1e8
Vinicius <vtmsugimoto@gmail.com>
2019-04-09 12:49:09 -0300

    Emoji macaco

--- Macacos/caminhoDaSelva.c
+++ Macacos/caminhoDaSelva.c
@@ -2,18 +2,24 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
 
 #define L 30
 #define lock(mutex) pthread_mutex_lock(mutex)
 #define unlock(mutex) pthread_mutex_unlock(mutex)
 
+//pthread_mutex_t portaDoBando[] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
 pthread_mutex_t portaDoBandoUm = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t portaDoBandoDois = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t portaDoFiscal = PTHREAD_MUTEX_INITIALIZER;
 
 void * caminhoDaSelva(void *);
+void imprimeMacaco(int direcao);
 
 int main(void){
+  setlocale(LC_ALL, "");
+  imprimeMacaco(1);
   pthread_t thds[L];
   for(int i = 0; i < L; i++){
     int *info = (int *) malloc(sizeof(int) * 2);
@@ -39,7 +45,7 @@ void * caminhoDaSelva(void *arg1){
       unlock(&portaDoBandoUm);
       unlock(&portaDoFiscal);
 
-      printf("Macaco %d do bando %d passando. Uh-Uhh!\n", id, bando);
+      printf("\e[7;35mMacaco %d do bando %d passando. Uh-Uhh!\e[0m\n", id, bando);
       sleep(1);
 
       lock(&portaDoBandoUm);
@@ -55,7 +61,7 @@ void * caminhoDaSelva(void *arg1){
       unlock(&portaDoBandoDois);
       unlock(&portaDoFiscal);
 
-      printf("Macaco %d do bando %d passando. Uh-Uhh!\n", id, bando);
+      printf("\e[7;36mMacaco %d do bando %d passando. Uh-Uhh!\e[0m\n", id, bando);
       sleep(1);
 
       lock(&portaDoBandoDois);
@@ -66,4 +72,11 @@ void * caminhoDaSelva(void *arg1){
   }
 
   pthread_exit(NULL);
+}
+void imprimeMacaco(int direcao){
+  for(int i = 0; i < 4; i++){
+    printf("%s%ls", i == 0 ? "" : "\b ", L"🐵");
+    sleep(1);
+  }
+  printf("\n");
 }
