05587e6b72efbef4bac7fa1701c2b625243b9091
Vinícius T M Sugimoto <vtmsugimoto@gmail.com>
2019-04-12 15:09:08 -0300

    Produtores e consumidores produzindo, consumindo, dormindo, acordando e colorindo adequadamente

--- Produtor e Consumidor/produtorEConsumidor.c
+++ Produtor e Consumidor/produtorEConsumidor.c
@@ -3,15 +3,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <wchar.h>
+#include <locale.h>
 
 pthread_mutex_t porteira = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t naoTemOQueFazer = PTHREAD_COND_INITIALIZER;
+pthread_cond_t semEspaco = PTHREAD_COND_INITIALIZER;
+pthread_cond_t semProdutos = PTHREAD_COND_INITIALIZER;
 
 #define N 10
 #define lock(porta) pthread_mutex_lock(porta)
 #define unlock(porta) pthread_mutex_unlock(porta)
-#define sleep(cond, lock) pthread_cond_wait(cond, lock);
-#define wake(cond) pthread_cond_signal(cond);
+#define wait(cond, lock) pthread_cond_wait(cond, lock);
+#define signal(cond) pthread_cond_signal(cond);
 
 int buffer[10] = {[0 ... 9] = 0};
 int contador = 0;
@@ -20,6 +23,7 @@ void * produtor(void *arg);
 void * consumidor(void *arg);
 
 int main(void){
+  setlocale(LC_ALL, "");
   srand(time(NULL));
   pthread_t thds[N];
   for(int i = 0; i < N; i++){
@@ -34,17 +38,21 @@ void * produtor(void *arg){
 
   while(1){
     lock(&porteira);
-    while(contador == N - 1){
-      sleep(&naoTemOQueFazer, &porteira);
+    while(contador == N){
+      printf("[\e[48;5;33m  \e[0m] \e[38;5;27mProdutor indo dormir, vitrine cheia.\e[0m\n");
+      wait(&semEspaco, &porteira);
     }
+    vitrine[contador] = rand() % 100;
+    printf("[\e[48;5;33m  \e[0m] \e[38;5;45mProduzido %d, colocado na %ls %d da vitrine\e[0m\n", vitrine[contador], L"posição", contador);
+    contador++;
     unlock(&porteira);
 
-     vitrine[contador] = rand() % 100;
-     printf("Produzido %d, %d\n", vitrine[contador], contador);
+    //sleep(3);
 
     lock(&porteira);
-    if(++contador == 1)
-       wake(&naoTemOQueFazer);
+    if(contador == 1)
+       printf("[\e[48;5;33m  \e[0m] \e[38;5;87mAcordando consumidor, vitrine %ls vazia\e[0m\n", L"não");
+       signal(&semProdutos);
     unlock(&porteira);  
   }
 
@@ -56,16 +64,20 @@ void * consumidor(void *arg){
 
   while(1){
     lock(&porteira);
-    while(!contador){
-      sleep(&naoTemOQueFazer, &porteira);
+    while(contador == 0){
+      printf("[\e[48;5;34m  \e[0m] \e[38;5;28mConsumidor indo dormir, vitrine vazia\e[0m\n");
+      wait(&semProdutos, &porteira);
     }
+    contador--;
+    printf("[\e[48;5;34m  \e[0m] \e[38;5;46mConsumido %d, pego da %ls %d da vitrine\e[0m\n", vitrine[contador], L"posição", contador);
     unlock(&porteira);
-
-    printf("Consumido %d, %d\n", vitrine[contador], contador);
+ 
+    //sleep(3);
 
     lock(&porteira);
-    if(--contador == 0)
-       wake(&naoTemOQueFazer);
+    if(contador == N - 1)
+       printf("[\e[48;5;34m  \e[0m] \e[38;5;70mAcordando produtor, %ls liberado.\e[0m\n", L"espaço");
+       signal(&semEspaco);
     unlock(&porteira);  
   }
 
